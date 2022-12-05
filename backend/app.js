3221c2d78e01a5c2aae2d9cf0f8056c5deba2b2f
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2022-12-05 14:51:10 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -15,21 +15,21 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const allowedCors = {
-	origin: [
-		'http://localhost:3000',
-		'https://localhost:3000',
-		'http://localhost:3001',
-		'https://localhost:3001',
-		'http://snv.mesto.nomoredomains.club',
-		'https://snv.mesto.nomoredomains.club',
-		'http://api.snv.mesto.nomoredomains.club',
-		'https://api.snv.mesto.nomoredomains.club',
-	],
-	methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-	preflightContinue: false,
-	optionsSuccessStatus: 204,
-	allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'cookies'],
-	credentials: true,
+  origin: [
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'http://localhost:3001',
+    'https://localhost:3001',
+    'http://snv.mesto.nomoredomains.club',
+    'https://snv.mesto.nomoredomains.club',
+    'http://api.snv.mesto.nomoredomains.club',
+    'https://api.snv.mesto.nomoredomains.club',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'cookies'],
+  credentials: true,
 };
 
 app.use('*', cors(allowedCors));
@@ -48,5 +48,5 @@ app.use(errors());
 app.use(serverError);
 
 app.listen(PORT, () => {
-	console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,27 +7,27 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const AuthError401 = require('../errors/authError');
 
 module.exports = (req, res, next) => {
-	const token = req.cookies.jwt;
+  const token = req.cookies.jwt;
 
-	//console.log(token);
+  // console.log(token);
 
-	if (!token) {
-		return next(new AuthError401('Необходима авторизация'));
-	}
+  if (!token) {
+    return next(new AuthError401('Необходима авторизация'));
+  }
 
-	let payload;
+  let payload;
 
-	try {
-		payload = jsonwebtoken.verify(
-			token,
-			// eslint-disable-next-line comma-dangle
-			NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-		);
-	} catch (err) {
-		return next(new AuthError401('Необходима авторизация'));
-	}
+  try {
+    payload = jsonwebtoken.verify(
+      token,
+      // eslint-disable-next-line comma-dangle
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+    );
+  } catch (err) {
+    return next(new AuthError401('Необходима авторизация'));
+  }
 
-	req.user = payload;
+  req.user = payload;
 
-	return next();
+  return next();
 };
