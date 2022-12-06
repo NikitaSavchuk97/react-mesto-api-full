468d2ff7b5257deb1d50e8a7a4f618469d300637
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2022-12-06 08:35:13 +0300

    fix

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -7,12 +7,6 @@ const auth = require('../middlewares/auth');
 const routerUsers = require('./users');
 const routerCards = require('./cards');
 
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 router.post('/signup', createUserValidation, createUser);
 router.post('/signin', loginUserValidation, loginUser);
 
@@ -21,11 +15,11 @@ router.use(routerUsers);
 router.use(routerCards);
 
 router.get('/signout', (req, res) => {
-  res.clearCookie('jwt').send({ message: 'Выход' });
+	res.clearCookie('jwt').send({ message: 'Выход' });
 });
 
 router.use((req, res, next) => {
-  next(new NotFoundError('Запрашиваемая страница не существует'));
+	next(new NotFoundError('Запрашиваемая страница не существует'));
 });
 
 module.exports = router;
