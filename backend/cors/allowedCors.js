185d846b64a98cb0079df3f547e6d8748ffa22a3
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2022-12-06 08:31:41 +0300

    fix

--- backend/cors/allowedCors.js
+++ backend/cors/allowedCors.js
@@ -1,19 +1,19 @@
 const allowedCors = {
-  origin: [
-    'http://localhost:3000',
-    'https://localhost:3000',
-    'http://localhost:3001',
-    'https://localhost:3001',
-    'http://snv.mesto.nomoredomains.club',
-    'https://snv.mesto.nomoredomains.club',
-    'http://api.snv.mesto.nomoredomains.club',
-    'https://api.snv.mesto.nomoredomains.club',
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'cookies'],
-  credentials: true,
+	origin: [
+		'http://localhost:3000',
+		'https://localhost:3000',
+		'http://localhost:3001',
+		'https://localhost:3001',
+		'http://snv.mesto.nomoredomains.club',
+		'https://snv.mesto.nomoredomains.club',
+		'http://api.snv.mesto.nomoredomains.club',
+		'https://api.snv.mesto.nomoredomains.club',
+	],
+	methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+	preflightContinue: false,
+	optionsSuccessStatus: 204,
+	allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'cookies'],
+	credentials: true,
 };
 
 module.exports = allowedCors;
