cf8154b6f3e158673ad97f2fd76c217afeb29056
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2023-02-06 11:43:58 +0300

    fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,27 +7,28 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const AuthError401 = require('../errors/authError');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+	const token = req.cookies.jwt;
+	console.log(token);
 
-  // console.log(token);
+	// console.log(token);
 
-  if (!token) {
-    return next(new AuthError401('Необходима авторизация 1'));
-  }
+	if (!token) {
+		return next(new AuthError401('Необходима авторизация 1'));
+	}
 
-  let payload;
+	let payload;
 
-  try {
-    payload = jsonwebtoken.verify(
-      token,
-      // eslint-disable-next-line comma-dangle
-      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-    );
-  } catch (err) {
-    return next(new AuthError401('Необходима авторизация 2'));
-  }
+	try {
+		payload = jsonwebtoken.verify(
+			token,
+			// eslint-disable-next-line comma-dangle
+			NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+		);
+	} catch (err) {
+		return next(new AuthError401('Необходима авторизация 2'));
+	}
 
-  req.user = payload;
+	req.user = payload;
 
-  return next();
+	return next();
 };
