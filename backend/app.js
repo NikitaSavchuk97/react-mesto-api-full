86b2911d1cfc973e3f1ca089968a6ee68ea356f9
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2023-02-16 19:25:00 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,41 @@
 require('dotenv').config();
-
 const cors = require('cors');
-const express = require('express');
 
-const { PORT = 3000 } = process.env;
+const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const allowedCors = require('./cors/allowedCors');
+
 const serverError = require('./middlewares/serverError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const {
+	PORT = 3000,
+	NODE_ENV,
+	MESTO_DB,
+	DEFAULT_DB = 'mongodb://localhost:27017/mesto-db-local',
+} = process.env;
+
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.set('strictQuery', false);
+mongoose.connect(NODE_ENV === 'production' ? MESTO_DB : DEFAULT_DB);
 
 app.use('*', cors(allowedCors));
-
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
 app.use(require('./routes/router'));
 
+
+
 app.use(errorLogger);
 app.use(errors());
 app.use(serverError);
+
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+	console.log(`App listening on port ${PORT}`);
 });
